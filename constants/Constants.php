1129d3b4db436c68dc33832910f2a3bedd352b1d
vikas1993 <vk.bkbiet@gmail.com>
2017-02-17 19:44:36 +0530

    Authorise Payment Api
    Check payment profile folder and create profile folder for for varius
    payment api

---
+++ constants/Constants.php
@@ -0,0 +1,21 @@
+<?php
+namespace SampleCode;
+
+class Constants
+{
+	//merchant credentials
+	const MERCHANT_LOGIN_ID = "7xT4dJp9d";
+	const MERCHANT_TRANSACTION_KEY = "8T66amm59mCKr2sc";
+	
+	const RESPONSE_OK = "Ok";
+	
+	//Recurring Billing
+	const SUBSCRIPTION_ID_GET = "2930242";
+
+	//Transaction Reporting
+	const TRANS_ID = "2238968786";
+
+	const SAMPLE_AMOUNT = "2.23";
+}
+?>
+
